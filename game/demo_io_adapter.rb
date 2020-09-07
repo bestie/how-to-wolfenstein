03fa93cfb207c85273c462e6e3d920105448893b
Stephen Best <bestie@gmail.com>
2020-09-07 16:31:46 +0200

    Automated demo

---
+++ game/demo_io_adapter.rb
@@ -0,0 +1,19 @@
+class DemoIOAdapter
+  def initialize(io, char_stream, delay: 1.0)
+    @io = io
+    @char_stream = char_stream
+    @delay = delay
+    @cursor = 0
+  end
+
+  def write(s)
+    @io.write(s)
+  end
+
+  def getch(*args)
+    char = @char_stream[@cursor]
+    sleep(@delay)
+    @cursor += 1
+    char || ANSI.ctrl_c
+  end
+end
