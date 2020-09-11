7c5d3c3621973d3e26322b6537d61d9c04a28592
Stephen Best <bestie@gmail.com>
2020-09-12 01:27:23 +0200

    Fix demos

--- demos/gradient_demo.rb
+++ demos/gradient_demo.rb
@@ -1,16 +1,27 @@
 require "io/console"
-require_relative "game/ansi"
+require_relative "../game/ansi"
 
-GRADIENT = ".-=+*#@%"
+GRADIENT = ".-=+*#@%".chars
+WALL_GRADIENT = (
+  GRADIENT.map { |c| ANSI.red(c) } +
+  GRADIENT.reverse.map { |c| ANSI.black_on_red(c) } +
+  [ANSI.black_on_red(" ")]
+)
 
 IO.console.raw do |io|
   height, width = io.winsize
 
-  gradation_size = (height / GRADIENT.length).floor
+  gradation_size = (height / WALL_GRADIENT.length).floor
+  display_size = gradation_size * WALL_GRADIENT.length
+  padding = height - display_size
 
-  height.times do |i|
+  padding.times do
+    io.write(" " * width + "\r\n")
+  end
+
+  display_size.times do |i|
     index = (i / gradation_size).floor
-    char = GRADIENT[index]
+    char = WALL_GRADIENT.fetch(index)
     io.write(char * width + "\r\n")
   end
 
